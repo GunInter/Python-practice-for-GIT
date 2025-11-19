a81a3b7bd7140bbb93f58f2c0b013664614d4a95
GunInter <73353279+GunInter@users.noreply.github.com>
2025-11-19 22:22:47 +0700

    Day7
    Today im study about For and nested loops and practice some question about this topic.

---
+++ 12.1 for loops practice.py
@@ -0,0 +1,8 @@
+prices = [10, 20, 30]
+final = 0
+
+for total in prices:
+
+    final += total
+
+print(final)
---
+++ 13.1Nest loop practice.py
@@ -0,0 +1,10 @@
+numbers = [5, 2, 5, 2, 2]
+
+for count_X in numbers:
+    output = ''
+
+    for count in range(count_X):
+
+        output += 'L'
+
+    print(output)
