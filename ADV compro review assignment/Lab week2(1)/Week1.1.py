bc1b7fe2c43987ba993fb7bac73ccdd35bf8a43f
GunInter <73353279+GunInter@users.noreply.github.com>
2025-11-23 19:31:34 +0700

    Adv compro review
    Practice python question by using old lab week from 2nd year

---
+++ ADV compro review assignment/Lab week2(1)/Week1.1.py
@@ -0,0 +1,18 @@
+# Assignment 1: Variable Types
+# Objective: Understand and utilize different variable types in Python.
+
+# 1.    Define an integer variable named age and set it to 25.
+# 2.    Define a float variable named height and set it to 5.9.
+# 3.    Define a string variable named name and set it to your name.
+# 4.    Define a boolean variable named is_student and set it to True.
+# 5.    Print all the variables.
+
+age = 20
+height = 173.5
+name = 'Gun'
+is_student = True
+
+print(age)
+print(height)
+print(name)
+print(is_student)
---
+++ ADV compro review assignment/Lab week2(1)/week1.2.py
@@ -0,0 +1,17 @@
+# Assignment 2: String Manipulation
+# Objective: Learn various string operations and methods.
+# 1.    Create a string variable named sentence with the value “Python programming is fun”.
+# 2.    Convert the string to uppercase and print it.
+# 3.    Replace the word “fun” with “awesome” and print the new sentence.
+# 4.    Split the sentence into a list of words and print it.
+
+course = "Python programming is fun"
+course_covert = course.upper()
+print(course_covert)
+
+
+replace_course = course.replace("fun", "awesome")
+print(replace_course)
+
+course_split = course.split()
+print(course_split)
