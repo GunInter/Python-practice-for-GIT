cd2e7e827c8e2aa0e7f841ec4e546c6950594189
GunInter <73353279+GunInter@users.noreply.github.com>
2025-11-25 01:23:06 +0700

    Adv compro review

--- ADV compro review assignment/Lab week2(1)/week1.3.py
+++ ADV compro review assignment/Lab week2(1)/week1.3.py
@@ -1,11 +1,11 @@
-#Assignment 3: List append() Method
-#Objective: Understand how to add elements to a list using append().
+# Assignment 3: List append() Method
+# Objective: Understand how to add elements to a list using append().
 
-    #1.    Create an empty list named colors.
-    #2.    Use the append() method to add “red”, “green”, and “blue” to the list.
-    #3.    Print the list.
-    #4.    Append “yellow” and “purple” to the list.
-    #5.    Print the length of the list.
+#   1.    Create an empty list named colors.
+#   2.    Use the append() method to add “red”, “green”, and “blue” to the list.
+#   3.    Print the list.
+#   4.    Append “yellow” and “purple” to the list.
+#   5.    Print the length of the list.
 
 color = []
 color.append("red")
---
+++ ADV compro review assignment/Lab week2(1)/week1.4.py
@@ -0,0 +1,11 @@
+"""Assignment 4: List remove() Method
+Objective: Learn how to remove specific elements from a list using remove().
+
+    1.    Create a list named animals containing “cat”, “dog”, “rabbit”, “bird”, “fish”.
+    2.    Use the remove() method to remove “rabbit” from the list.
+    3.    Print the list.
+"""
+animals = ["cat", "dog", "rabbit", "bird", "fish"]
+animals.remove("rabbit")
+
+print(animals)
