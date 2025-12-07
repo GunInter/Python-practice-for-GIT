a8d170f44e8cfd77862ed51699c2a0b9a77c3de2
GunInter <73353279+GunInter@users.noreply.github.com>
2025-12-08 02:00:47 +0700

    Doing hw week3 not lab

---
+++ Homework Week03/HW3.py
@@ -0,0 +1,12 @@
+'''
+**Homework 3: Adding Dictionary Items in a List of Dictionaries**
+Objective: Practice adding items in dictionaries within a list.
+1. Using data from the homework 2.
+2. Add a new key "graduation_year" with a value to each student's dictionary. Assume all students are expected to graduate in 2024.
+3. Remove the key "age" from each student's dictionary.
+4. Print the updated list of students.
+    Expected Output:
+    {'name': 'Alice', 'grade': 85, 'major': 'Physics', 'graduation_year': 2024}
+    {'name': 'Bob', 'grade': 90, 'major': 'Chemistry', 'graduation_year': 2024}
+    {'name': 'Charlie', 'grade': 78, 'major': 'Mathematics', 'graduation_year': 2024}
+    {'name': 'Diana', 'grade': 92, 'major': 'Biology', 'graduation_year': 2024}'''
