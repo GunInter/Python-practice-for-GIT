f12f2362679e67c31ed7307efb76cc0caa18db83
GunInter <73353279+GunInter@users.noreply.github.com>
2025-12-10 02:10:28 +0700

    Day8
    I learn about list and tmr will be 2d list and practice question.

---
+++ Python review learning course/14.1.list practice question.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 6, 8, 9, 4, 5]
+max = numbers[0]
+
+for largest in numbers:
+
+    if largest > max:
+        max = largest
+
+print(max)
---
+++ Python review learning course/14.Lists.py
@@ -0,0 +1,6 @@
+names = ['Honey', 'Gun', 'Zen', 'Tonnam', 'Parn']
+names[2] = 'Zenkung'
+print(names)
+print(names[2])
+print(names[-1])
+print(names[1:4])
