2dd23eb463044d7365f01ee58b17e0673b9edba5
GunInter <73353279+GunInter@users.noreply.github.com>
2025-12-29 15:51:59 +0700

    Day0.5(day1)
    I doing excercise

---
+++ Asabeneh-30-Days-Of-Python/Day1_excercise.py
@@ -0,0 +1,22 @@
+# 1.
+print(3+4)
+print(3-4)
+print(3*4)
+print(3 % 4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+name = "Gun"
+Family_name = "Kachonnarongvanich"
+my_country = "Thailand"
+Python = "I am enjoying 30 days of python"
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(Family_name))
+print(type(my_country))
