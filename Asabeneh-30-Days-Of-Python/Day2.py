77b9c548dfeb6cd32e4c531bce5c93ac6d5cc973
GunInter <73353279+GunInter@users.noreply.github.com>
2026-01-03 02:22:05 +0700

    Day2.py
    I doing a excersice about str and variable, rn is half of it so tmri will finish all of it.

---
+++ Asabeneh-30-Days-Of-Python/Day2.py
@@ -0,0 +1,26 @@
+first_name = "Nonpravit"
+last_name = "Kachonnarongvanich"
+full_name = "Nonpravit Kachonnarongvanich"
+country = "Thailand"
+city = "Bangkok"
+age = 21
+birth_year = 2004
+is_married = False
+is_true = True
+is_light_on = True
+
+first_name, last_name, full_name = "Nonpravit", "Kachonnarongvanich", "Nonpravit Kachonnarongvanich"
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(birth_year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print(len(last_name))
+print("Is first name longer than last name?", len(first_name) > len(last_name))
