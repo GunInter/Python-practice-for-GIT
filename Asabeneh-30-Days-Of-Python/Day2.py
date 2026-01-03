34e8046655bf827490642e55687d387ff31c2c80
GunInter <73353279+GunInter@users.noreply.github.com>
2026-01-03 20:57:01 +0700

    day2.py
    I finish all of my practice question all by myself and a little bit google help, no AI help btw.  Today I did it about string, input ,variable ,how to change type of info and lenght of the string.

--- Asabeneh-30-Days-Of-Python/Day2.py
+++ Asabeneh-30-Days-Of-Python/Day2.py
@@ -24,3 +24,23 @@ print(type(is_light_on))
 print(len(first_name))
 print(len(last_name))
 print("Is first name longer than last name?", len(first_name) > len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius_circle = 30
+pi = 3.14
+area_of_the_circle = pi * 30**2
+circum_of_circle = 2 * pi * radius_circle
+
+radius_input = input("Give the radius value: ")
+radius_input_int = int(radius_input)
+print(radius_input_int**2 * pi)
