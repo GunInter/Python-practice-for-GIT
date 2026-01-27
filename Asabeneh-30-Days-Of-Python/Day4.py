e379241e1bba1e0d88b3515d6e97ee80d2f367d6
GunInter <73353279+GunInter@users.noreply.github.com>
2026-01-28 02:15:11 +0700

    day4
    today i back for practice question ,i have been busy to my midterm exam. So today doing 5-6 question about use function to change string in many form like upper and lower case.

--- Asabeneh-30-Days-Of-Python/Day4.py
+++ Asabeneh-30-Days-Of-Python/Day4.py
@@ -17,17 +17,29 @@ c = 'All'
 
 merge1 = a + space + b + space + c
 print(merge1)
+
 # 3 Declare a variable named company and assign it to an initial value "Coding For All".
+
+company = merge1
 
 # 4 Print the variable company using print().
 
+print(company)
+
 # 5 Print the length of the company string using len() method and print().
+
+print(len(company))
 
 # 6 Change all the characters to capital letters using upper() method.
 
+print(company.upper())
+
 # 7 Change all the characters to lowercase letters using lower() method.
 
+print(company.lower())
+
 # 8 Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+
 
 # 9 Cut (slice) out the first word of Coding For All string.
 
