480dd8fe7e79eee93734d44f5e4f296abf250a39
GunInter <73353279+GunInter@users.noreply.github.com>
2026-04-16 02:34:32 +0700

    Review day2
    review

--- Asabeneh-30-Days-Of-Python/Day1_excercise.py
+++ Asabeneh-30-Days-Of-Python/Day1_excercise.py
@@ -1,4 +1,13 @@
-# 1.
+'''
+Open the python interactive shell and do the following operations. The operands are 3 and 4. Check the example above
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//)
+'''
 print(3+4)
 print(3-4)
 print(3*4)
@@ -7,16 +16,35 @@ print(3/4)
 print(3**4)
 print(3//4)
 
-name = "Gun"
-Family_name = "Kachonnarongvanich"
-my_country = "Thailand"
-Python = "I am enjoying 30 days of python"
+'''
+Write strings on the python interactive shell. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+'''
+name = "Nonpravit"
+famname = "Kachonnarongvanich"
+country = "Thailand"
+python = "I am enjoying 30 days of python"
 
+'''
+Check the data types of the following data:
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']
+Your name
+Your family name
+Your country
+'''
 print(type(10))
 print(type(9.8))
 print(type(3.14))
 print(type(4-4j))
 print(type(['Asabeneh', 'Python', 'Finland']))
 print(type(name))
-print(type(Family_name))
-print(type(my_country))
+print(type(famname))
+print(type(country))
+print(type(python))
