9c73b48685f4febad4b891cdd05569223f7376c2
GunInter <73353279+GunInter@users.noreply.github.com>
2026-04-29 01:52:05 +0700

    Day5.1.py

--- Asabeneh-30-Days-Of-Python/test.py
+++ Asabeneh-30-Days-Of-Python/test.py
@@ -1,8 +1,9 @@
-print('I hope everyone is enjoying the Python Challenge.\nAre you ?')  # line break
-print('Days\tTopics\tExercises')
-print('Day 1\t3\t5')
-print('Day 2\t3\t5')
-print('Day 3\t3\t5')
-print('Day 4\t3\t5')
-print('This is a backslash  symbol (\\)')  # To write a backslash
-print('In every programming language it starts with \"Hello, World!\"')
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[0:4]  # it returns all the fruits
+# this will also give the same result as the one above
+all_fruits = fruits[0:]  # if we don't set where to stop it takes all the rest
+orange_and_mango = fruits[1:3]  # it does not include the first index
+orange_mango_lemon = fruits[1:]
+# here we used a 3rd argument, step. It will take every 2cnd item - ['orange', 'lemon']
+orange_and_lemon = fruits[::2]
+print(orange_and_mango)
