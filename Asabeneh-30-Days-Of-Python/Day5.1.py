ee87514d637229feb8662a2283d3fc08ac3e8d24
GunInter <73353279+GunInter@users.noreply.github.com>
2026-04-30 02:24:25 +0700

    Update Day5.1.py

---
+++ Asabeneh-30-Days-Of-Python/Day5.1.py
@@ -0,0 +1,89 @@
+# 1. Declare an empty list
+empty_list = []
+# 2. Declare a list with more than 5 items
+stocks =['Amazon','Nvdia','Blackrock','Netflix','Microsoft' ]
+# 3. Find the length of your list
+print(len(stocks))
+# 4. Get the first item, the middle item and the last item of the list
+First = (stocks[0])
+Middle = stocks[len(stocks)//2]
+Last = (stocks[-1])
+print(First)
+print(Middle)
+print(Last)
+# 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Nonpravit','21','174','GF','105/47 Pinkloa city village']
+
+# 6. Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ['Facebook','Google','Apple','IBM','Oracle','Amazon']
+# 7. Print the list using print()
+print(it_companies)
+# 8. Print the number of companies in the list
+number_company = (len(it_companies))
+print(number_company)
+# 9. Print the first, middle and last company
+First_company = (it_companies[0])
+Middle_company = it_companies[len(stocks)//2]
+Last_company = (it_companies[-1])
+print(First_company)
+print(Middle_company)
+print(Last_company)
+
+# 10. Print the list after modifying one of the companies
+
+# 11. Add an IT company to it_companies
+
+# 12. Insert an IT company in the middle of the companies list
+
+# 13. Change one of the it_companies names to uppercase (IBM excluded!)
+
+# 14. Join the it_companies with a string '#;  '
+
+# 15. Check if a certain company exists in the it_companies list.
+
+# 16. Sort the list using sort() method
+
+# 17. Reverse the list in descending order using reverse() method
+
+# 18. Slice out the first 3 companies from the list
+
+# 19. Slice out the last 3 companies from the list
+
+# 20. Slice out the middle IT company or companies from the list
+
+# 21. Remove the first IT company from the list
+
+# 22. Remove the middle IT company or companies from the list
+
+# 23. Remove the last IT company from the list
+
+# 24. Remove all IT companies from the list
+
+# 25. Destroy the IT companies list
+
+# 26. Join the following lists:
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+# back_end = ['Node','Express', 'MongoDB']
+
+# 27. After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
+
+# 28. The following is a list of 10 students ages:
+# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# Sort the list and find the min and max age
+
+# 29. Add the min age and the max age again to the list
+
+# 30. Find the median age (one middle item or two middle items divided by two)
+
+# 31. Find the average age (sum of all items divided by their number )
+
+# 32. Find the range of the ages (max minus min)
+
+# 33. Compare the value of (min - average) and (max - average), use abs() method
+
+# 34. Find the middle country(ies) in the countries list
+
+# 35. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+
+# 36. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark'].
+# Unpack the first three countries and the rest as scandic countries.
