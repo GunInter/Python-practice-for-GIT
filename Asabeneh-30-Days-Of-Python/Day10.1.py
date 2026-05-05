e28869ffec73c3dc2bf1af28519e245ad5a60df4
GunInter <73353279+GunInter@users.noreply.github.com>
2026-05-06 02:13:22 +0700

    loop

---
+++ Asabeneh-30-Days-Of-Python/Day10.1.py
@@ -0,0 +1,58 @@
+# loops exercises
+
+# 1. Iterate 0 to 10 using for loop
+for number in range(11):
+    print(number)
+# 2. Iterate 0 to 10 using while loop
+count = 0
+while count < 11:
+    print(count)
+    count = count + 1
+# 3. Iterate 10 to 0 using for loop
+for number in range(10, 1, -1):
+    print(number)
+# 4. Iterate 10 to 0 using while loop
+count = 10
+while count >= 0:
+    print(count)
+    count = count - 1
+# 5. Print triangle pattern:
+# #
+# ##
+# ###
+# ####
+# #####
+# ######
+# #######
+
+# 6. Nested loop pattern (8x8 grid)
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+# # # # # # # #
+
+# 7. Print multiplication squares:
+# 0 x 0 = 0
+# ...
+# 10 x 10 = 100
+
+# 8. Iterate list and print items
+lst = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
+
+# 9. Print even numbers from 0 to 100
+
+# 10. Print odd numbers from 0 to 100
+
+# 11. Print sum of all numbers from 0 to 100
+
+# 12. Print sum of evens and odds separately
+
+# 13. Loop countries and find those containing 'land'
+# (you can mock a list if no file)
+
+# 14. Reverse fruit list using loop
+fruits = ['banana', 'orange', 'mango', 'lemon']
